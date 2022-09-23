9872a2b56b21b7278bd3d488de10e7a82fe0c420
hrJin9 <bushyerin97@gmail.com>
2022-09-23 16:48:16 +0900

    혜린 전부 삭제해서 내 브랜치로 푸시함.

--- Semi_proj/src/main/java/Jihee/Jihee.java
+++
@@ -1,5 +0,0 @@
-package Jihee;
-
-public class Jihee {
-
-}
--- Semi_proj/src/main/java/hyerin/retry/Hyerin_retry.java
+++
@@ -1,9 +0,0 @@
-package hyerin.retry;
-
-public class Hyerin_retry {
-
-	public static void main(String[] args) {
-		System.out.println("다시시도");
-	}
-
-}
